f594fa2067f4f669d92dc78911f7c4306e55130a
markhopwoodnvm <mark.hopwood.nvm@gmail.com>
2017-07-03 12:42:29 +0000

    Fix data scourse config

--- TESTCI/webserver/data.tf
+++ TESTCI/webserver/data.tf
@@ -4,7 +4,7 @@ data "terraform_remote_state" "vpc" {
   config {
     region = "eu-west-2"
     bucket = "tabi-terraformpipeline" 
-    key = "SPIKE3/DEV/vpc/terraform.tfstate"
+    key = "SPIKE3/TESTCI/vpc/terraform.tfstate"
     profile = "developmentaws"     
   }
 }
